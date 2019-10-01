9d7ef6fa3bb1f51c6f65d13131e226f5d5733041
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-10-01 22:54:45 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ if __name__=="__main__":
     print("===========================")
 
     for fl in materials:
+        fln=re.sub(r'[/\\]顔[/\\]',"/他/face_",fl)
+        fln=re.sub(r'[/\\]後[/\\]',"/他/back_",fln)
+        fln=re.sub(r'[/\\]全[/\\]',"/他/all_",fln)
+        fln=re.sub(r'[/\\]服上[/\\]',"/他/clothes1_",fln)
+        fln=re.sub(r'[/\\]服下[/\\]',"/他/clothes2_",fln)
+        fln=re.sub(r'[/\\]髪[/\\]',"/他/Hairstyle_",fln)
+        if fl!=fln:shutil.move(fl,fln);fl=fln
+
         if "." not in fl:continue
         if fl.split(".")[-1] not in ["bmp","jpg","jpeg","png"]:continue
         fln="".join(fl.split(".")[:-1])
@@ -30,6 +38,6 @@ if __name__=="__main__":
 
         if re.compile('[a-z]').match(fln[-1]) is None:continue
         os.rename(fl,fln[:-1]+"."+str(1+ord(fln[-1])-ord('a'))+"."+fl.split(".")[-1])
-        print(fln[:-1]+"."+str(1+ord(fln[-1])-ord('a'))+"."+fl.split(".")[-1])
+
 
     print("Completed!")
